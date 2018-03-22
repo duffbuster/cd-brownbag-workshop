69f49e559d55d13d0970c5b90efe87f30bd5272f
Colin Mackey <colin.mackey@udig.com>
2018-03-21 20:19:38 -0400

    Formatting

--- scripts/test.js
+++ scripts/test.js
@@ -5,9 +5,9 @@ process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+// Makes the script crash on unhandled rejections instead of silently ignoring
+// them. In the future, promise rejections that are not handled will terminate
+// the Node.js process with a non-zero exit code.
 process.on('unhandledRejection', err => {
   throw err;
 });
@@ -16,12 +16,13 @@ process.on('unhandledRejection', err => {
 require('../config/env');
 
 const jest = require('jest');
-const argv = process.argv.slice(2);
+const argv = process
+  .argv
+  .slice(2);
 
 // Watch unless on CI or in coverage mode
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
-
 
 jest.run(argv);
